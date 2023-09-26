cbbe0c84509168614c95f4ea06d95fabeb530ad0
izhar9 <izharidrisi.51@gmail.com>
2023-09-26 22:06:04 +0530

    e-dashboard

---
+++ front-end/src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+const Footer = () => {
+  return (
+    <div className="footer">
+      <h3>Made with 😊 Izhar &copy; 2023</h3>
+    </div>
+  );
+};
+
+export default Footer;
